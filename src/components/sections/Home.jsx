a12801384eb2476fc07b9c5f85e5737d0cd9566d
Sophie <sophiie.tran@gmail.com>
2026-06-09 21:29:01 -0500

    class to className fix

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -2,26 +2,26 @@ export default function HomeSection() {
   return (
     <section
       id="home"
-      class="relative h-screen w-screen flex justify-center items-center bg-linear-to-t from-[rgba(190,98,41,0.1)] via-20% via-transparent"
+      className="relative h-screen w-screen flex justify-center items-center bg-linear-to-t from-[rgba(190,98,41,0.1)] via-20% via-transparent"
     >
-      <div class="text-center mx-12 mb-12 -mt-7">
-        <p class="text-xl">hello! my name is</p>
+      <div className="text-center mx-12 mb-12 -mt-7">
+        <p className="text-xl">hello! my name is</p>
 
-        <h1 class="font-bold text-[10rem] tracking-wider">SOPHIE TRAN</h1>
+        <h1 className="font-bold text-[10rem] tracking-wider">SOPHIE TRAN</h1>
 
-        <p class="text-2xl max-w-4xl mx-auto">
+        <p className="text-2xl max-w-4xl mx-auto">
           software engineer. front-end developer. located in
-          <span class="text-[#f1821a]"> dallas, texas</span>. passionate about
+          <span className="text-[#f1821a]"> dallas, texas</span>. passionate about
           creating impactful solutions.
         </p>
 
         {/* email + resume */}
-        <div class="group absolute bottom-45 left-0 right-0 flex flex-col items-center gap-4 text-[#797979] ">
-          <div class="flex flex-wrap justify-center items-center font-medium text-lg sm:flex-nowrap group-hover:text-[#ffffff]">
-            <p class="mr-2 transition-colors duration-300 ">get in touch: </p>
+        <div className="group absolute bottom-45 left-0 right-0 flex flex-col items-center gap-4 text-[#797979] ">
+          <div className="flex flex-wrap justify-center items-center font-medium text-lg sm:flex-nowrap group-hover:text-[#ffffff]">
+            <p className="mr-2 transition-colors duration-300 ">get in touch: </p>
             <a
               href="mailto:sophietrann01@gmail.com"
-              class="transition-colors duration-300 hover:text-[#f1821a]"
+              className="transition-colors duration-300 hover:text-[#f1821a]"
             >
               sophietrann01@gmail.com
             </a>
@@ -30,14 +30,14 @@ export default function HomeSection() {
           <a
             href="/sophie-tran-resume-swe.pdf"
             download
-            class="inline-flex items-center px-4 py-1 border-2 border-[#797979]
+            className="inline-flex items-center px-4 py-1 border-2 border-[#797979]
             font-medium text-lg transition-all duration-300 group-hover:text-[#ffffff] group-hover:border-[#ffffff] hover:text-[#f1821a] hover:border-[#f1821a]"
             aria-label="Download Sophie Tran's Resume"
           >
             <img
               src="/download-green.png"
               alt="Download icon"
-              class="mr-2 h-6"
+              className="mr-2 h-6"
             />
             Resume
           </a>
