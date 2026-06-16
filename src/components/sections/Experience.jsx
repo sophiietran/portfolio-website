976a04cc7a5ec465900bdcb66b84097b45be2b4b
Sophie <sophiie.tran@gmail.com>
2026-06-16 13:17:31 -0500

    data cleanup

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -4,7 +4,7 @@ export default function Experience() {
   return (
     <section
       id="experience"
-      className="w-screen pt-25 pb-15 px-6 flex flex-col justify-start items-center bg-linear-to-b from-[#10151d] to-[#10151d] via-[#f1821a37]"
+      className="w-screen py-30 px-6 flex flex-col justify-start items-center bg-linear-to-b from-[#10151d] to-[#10151d] via-[#f1821a37]"
     >
       <h1 className="text-6xl md:text-8xl font-medium leading-tight text-center md:text-right pb-15">
         EXPERIENCE
@@ -18,21 +18,6 @@ export default function Experience() {
           desc="Delivered Salesforce features across full SDLC environments through Copado CI/CD pipelines, including expanding a complaint management system with automated priority-based due dates."
           skills="Salesforce, Java, SQL"
         />
-        <Job
-          company="Comerica Bank"
-          title="Software Engineer Intern"
-          date="May - Aug 2025"
-          desc="Delivered Salesforce features across full SDLC environments through Copado CI/CD pipelines, including expanding a complaint management system with automated priority-based due dates."
-          skills="Salesforce, Java, SQL"
-        />
-        <Job
-          company="Comerica Bank"
-          title="Software Engineer Intern"
-          date="May - Aug 2025"
-          desc="Delivered Salesforce features across full SDLC environments through Copado CI/CD pipelines, including expanding a complaint management system with automated priority-based due dates."
-          skills="Salesforce, Java, SQL"
-        />
-        
       </div>
     </section>
   );
