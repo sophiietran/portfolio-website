296790d04fdb2ee5347322df95564ec9d92ae042
Sophie <sophiie.tran@gmail.com>
2026-06-16 13:17:56 -0500

    project section redesign

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,33 +1,57 @@
 export default function ProjectCard(props) {
   return (
-    <div class="relative bg-[#f2f2f4] text-[#778069] flex flex-col p-4 w-[375px] min-h-[285px] border border-[#b9b3ab] hover:bg-[#e9e7e3] transition-all duration-500 shadow-sm transform hover:-rotate-1 hover:-translate-y-1 hover:shadow-2xl">
-      {/* card link */}
-      <a href={props.github}>
-        {/* title */}
-        <h2 class="font-semibold text-xl mb-2">{props.title}</h2>
-        {/* description */}
-        <p class="text-[#9d9c91] font-regular text-md my-1 pt-2">{props.desc}</p>
+    <div
+      className={`flex flex-col lg:w-full lg:flex-row lg:gap-5 items-center lg:items-start ${props.reverse ? "lg:flex-row-reverse" : "lg:flex-row"}`}
+    >
+      {/* Image */}
+      <div className="relative w-full lg:w-145 lg:h-90 shrink-0">
+        <img
+          src={props.img}
+          alt={`${props.title} screenshot`}
+          className="w-full h-full object-cover lg:grayscale lg:brightness-75 lg:transition-all lg:duration-200 lg:hover:grayscale-0 lg:hover:brightness-100"
+        />
+      </div>
 
-        {/* skills and github */}
-        <div class="absolute bottom-4 left-4 right-5 flex justify-between items-center pt-4">
-          <ul class="flex flex-wrap text-sm font-light">
-            {props.skills.map((skill, index) => (
-              <li
-                key={index}
-                class="font-mono text-[#778069] px-2 pr-4 rounded-md"
-              >
-                {skill}
-              </li>
-            ))}
-          </ul>
+      {/* Content */}
+      <div className="flex flex-col text-center">
+        <h1 className={`font-semibold text-2xl ${props.reverse ? "lg:text-left" : "lg:text-right"} pt-4 pb-3 lg:pt-15`}>{props.title}</h1>
 
-          <img
-            src="/github-icon.svg"
-            alt="github logo"
-            class="place-self-end mt-auto mb-1 ml-1"
-          />
+        <div className={`bg-[#29354a] text-sm p-2 text-center ${props.reverse ? "lg:-mr-14 z-10" : "lg:-ml-14 z-10"}`}>{props.desc}</div>
+
+        <div className="flex justify-between items-center">
+          {/* icon links */}
+          <div className="flex flex-row gap-3 p-2">
+
+            <a href={props.github}>
+              <img
+                src="src/assets/github.svg"
+                alt="github"
+                className="w-7 h-7"
+              />
+            </a>
+
+            {props.link && (
+              <a href={props.link}>
+                <img src="src/assets/link.svg" alt="link" className="w-8 h-8" />
+              </a>
+            )}
+          </div>
+
+          {/* skills */}
+          {props.skills && (
+            <div className="flex flex-wrap gap-2 p-1 justify-start">
+              {props.skills.split(",").map((skill, index) => (
+                <span
+                  key={index}
+                  className="font-medium rounded-md py-1 px-1.5 text-xs text-[#6efaffc7] border"
+                >
+                  {skill.trim()}
+                </span>
+              ))}
+            </div>
+          )}
         </div>
-      </a>
+      </div>
     </div>
   );
 }
