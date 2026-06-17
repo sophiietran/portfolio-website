d96e4e3f12a26ab3334067795763832d69d50208
Sophie <sophiie.tran@gmail.com>
2026-06-17 11:39:55 -0500

    fix hamburger icon on header

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -30,17 +30,19 @@ export default function Header() {
       <header
         className={`fixed top-0 w-full flex items-center py-4 px-8 z-50 transition-all duration-500 ${
           isScrolled ? "bg-[#10151d]/70 backdrop-blur-md" : "bg-transparent"
-        } ${isMenuOpen ? "hidden md:flex" : "flex"}`}
+        } ${isMenuOpen ? "hidden min-[806px]:flex" : "flex"}`}
       >
-        <p className="text-[#6efaff]">sophietran.dev</p>
+        <a href="#home" className="text-[#f1821a]">
+          sophietran.dev
+        </a>
 
         {/* Desktop Nav */}
-        <nav className="hidden md:flex absolute left-1/2 -translate-x-1/2 space-x-10 text-center">
+        <nav className="hidden min-[806px]:flex absolute left-1/2 -translate-x-1/2 gap-15 text-center">
           {navLinks.map((link) => (
             <a
               key={link.name}
               href={link.href}
-              className="text-[#ffffff] hover:text-[#54c1c5] transition-colors duration-200"
+              className="text-[#61dde2] hover:text-[#bae7e9] transition-colors duration-200"
             >
               {link.name}
             </a>
@@ -49,7 +51,7 @@ export default function Header() {
 
         {/* Hamburger button */}
         <div
-          className={`md:hidden flex items-center z-50 ml-auto ${
+          className={`min-[806px]:hidden flex items-center z-50 ml-auto ${
             isMenuOpen ? "hidden" : "flex"
           }`}
         >
@@ -65,7 +67,7 @@ export default function Header() {
 
       {/* show close button when side menu is displayed */}
       {isMenuOpen && (
-        <div className="fixed top-4 right-8 z-50 md:hidden">
+        <div className="fixed top-4 right-8 z-50 min-[806px]:hidden">
           <button onClick={toggleNavMenu} aria-label="Close navigation menu">
             <img src="x-thin.svg" alt="close menu" className="h-8 w-8" />
           </button>
@@ -74,7 +76,7 @@ export default function Header() {
 
       {/* Mobile drawer */}
       <div
-        className={`fixed inset-y-0 right-0 bg-[#ffffff] text-[#131923] z-40 w-80 transform transition-transform duration-300 ease-in-out md:hidden ${
+        className={`fixed inset-y-0 right-0 bg-[#ffffff] text-[#131923] z-40 w-80 transform transition-transform duration-300 ease-in-out min-[806px]:hidden ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
         } flex flex-col items-left justify-baseline space-y-6 pt-30 pl-6`}
       >
